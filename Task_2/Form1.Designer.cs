a4d49ce669084331faca91d06097eacff11bdbbb
AVTahion <avkontakt40@gmail.com>
2019-02-16 13:23:52 +0300

    Задание 2 а

---
+++ Task_2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Task_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxUserAnswr = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxUserAnswr
+            // 
+            this.txtBoxUserAnswr.Location = new System.Drawing.Point(54, 68);
+            this.txtBoxUserAnswr.Name = "txtBoxUserAnswr";
+            this.txtBoxUserAnswr.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxUserAnswr.TabIndex = 0;
+            this.txtBoxUserAnswr.Text = "0";
+            this.txtBoxUserAnswr.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBoxUserAnswr_KeyDown);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(197, 68);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Играть";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 189);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtBoxUserAnswr);
+            this.Name = "Form1";
+            this.Text = "Угадай число";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxUserAnswr;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Task_2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    public partial class Form1 : Form
+    {
+        private int numb = 0;
+
+        /// <summary>
+        /// Метод генерирует случайное число от 1 до 100
+        /// </summary>
+        /// <returns></returns>
+        static int RndNumb()
+        {
+            int x = 0;
+            Random rnd = new Random();
+            return x = rnd.Next(1, 100);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtBoxUserAnswr.Text = "0";
+            MessageBox.Show("Угадайте число от 1 до 100");
+            numb = RndNumb();
+            
+        }
+
+        private void txtBoxUserAnswr_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (int.Parse(txtBoxUserAnswr.Text) == numb)
+                {
+                    MessageBox.Show($"Поздравляю вы угадали!");
+                }
+                else
+                {
+                    if (int.Parse(txtBoxUserAnswr.Text) > numb)
+                    {
+                        MessageBox.Show($"Загаданное число меньше, попробуйте еще раз");
+                    }
+                    else
+                    {
+                        if (int.Parse(txtBoxUserAnswr.Text) < numb)
+                        {
+                            MessageBox.Show($"Загаданное число больше, попробуйте еще раз");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
